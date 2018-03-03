4d3802d62439721de270ec489fea15d8090e47bd
Karl E. Jorgensen <karl@jorgensen.org.uk>
2018-03-03 20:38:47 +0000

    "Session" is now "Hub"

--- virginmedia_router.py
+++ virginmedia_router.py
@@ -8,6 +8,10 @@ class LoginFailed(IOError):
     def __init__(self, msg):
         IOError.__init__(self, msg)
 
+class AccessDenied(IOError):
+    def __init__(self, msg):
+        IOError.__init__(self, msg)
+
 def params(dict = None):
     result = {
         "_": int(round(time.time() * 1000)),
@@ -17,20 +21,28 @@ def params(dict = None):
         result.update(dict)
     return result
 
-class Session:
+class Hub:
 
-    def __init__(self, hostname):
+    def __init__(self, hostname='192.168.0.1', **kwargs):
 
         self._credential = None
         self._url = 'http://' + hostname
         self._hostname = hostname
+        self._username = None
+        self._password = None
+        if kwargs:
+            self.login(**kwargs)
 
     def _get(self, url, **kwargs):
         """Shorthand for requests.get"""
         if self._credential:
-            return requests.get(self._url + '/' + url, cookies={"credential": self._credential}, **kwargs)
+            r = requests.get(self._url + '/' + url, cookies={"credential": self._credential}, **kwargs)
         else:
-            return requests.get(self._url + '/' + url, **kwargs)
+            r = requests.get(self._url + '/' + url, **kwargs)
+        r.raise_for_status()
+        if r.status_code == 401:
+            raise AccessDenied(url)
+        return r
 
     def login(self, username="admin", password="admin"):
         """Log into the router.
@@ -38,7 +50,6 @@ class Session:
         This will capture the credentials to be used in subsequent requests
         """
         r = self._get('login', params = params( { "arg": base64.b64encode(username + ':' + password) } ) )
-        r.raise_for_status()
 
         if not r.content:
             raise LoginFailed("Unknown reason. Sorry. Headers were {h}".format(h=r.headers))
@@ -50,29 +61,31 @@ class Session:
             raise LoginFailed(r.content)
 
         self._credential = r.content
+        self._username = username
+        self._password = password
 
     def logout(self):
         if self._credential:
-            print "Logging out"
             self._credential = None
-            r = requests.get('%s/logout' % self._url,
-                                 params= params() )
+            self._username = None
+            self._password = None
+            self._get('/logout', params= params() )
 
     def __enter__(self):
+        """Context manager support: Called on the way in"""
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        """Context manager support: Called on the way out"""
         self.logout()
         return False
 
     def snmpGet(self, oid):
-        r = requests.get("%s/snmpGet" % self._url, params = params( { "oids": oid }  ))
-        print "Response", r
-        print "Headers", r.headers
-        print "Content", r.content
+        r = json.loads(self._get("snmpGet", params = { "oids": oid }).content)
+        return r[oid]
 
     def __str__(self):
-        return "Session(hostname=%s)" % self._hostname
+        return "Hub(hostname=%s, username=%s)" % (self._hostname, self._username)
 
     def __nonzero__(self):
         return (self._credential != None)
@@ -80,15 +93,28 @@ class Session:
     def __del__(self):
         self.logout()
 
-def __demo():
-    with Session(hostname = '192.168.0.1') as s:
-        print "Got", s
-        # print "foo", s.snmpGet("1.3.6.1.4.1.4115.1.20.1.1.1.17.0")
-        # print "bar", s.snmpGet("1.3.6.1.4.1.4115.1.20.1.1.1.13")
+    @property
+    def connectionType(self):
+        r = json.loads(self._get('checkConnType').content)
+        return r["conType"]
 
-        s.login(password='dssD04vy0z4t')
-        print "serial number", s.snmpGet("1.3.6.1.4.1.4115.1.20.1.1.5.8");
-        print "Would do stuff with", s
+
+def __demo():
+    with Hub(hostname = '192.168.0.1') as hub:
+        print "Got", hub
+        # print "foo", hub.snmpGet("1.3.6.1.4.1.4115.1.20.1.1.1.17.0")
+        # print "bar", hub.snmpGet("1.3.6.1.4.1.4115.1.20.1.1.1.13")
+
+#        hub.login(password='dssD04vy0z4t')
+        print "Connection type", hub.connectionType
+        for oid in [
+                "1.3.6.1.4.1.4115.1.20.1.1.1.17.0",
+                "1.3.6.1.4.1.4115.1.20.1.1.1.18.1.0",
+                "1.3.6.1.4.1.4115.1.20.1.1.5.14.0",
+                "1.3.6.1.4.1.4115.1.20.1.1.5.62.0",
+                "1.3.6.1.4.1.4491.2.1.14.1.5.4.0"
+                ]:
+            print oid, hub.snmpGet(oid)
 
 if __name__ == '__main__':
     __demo()
