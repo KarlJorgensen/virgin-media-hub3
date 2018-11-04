292818860320ffd158640354b8bf517bd835d1c8
Karl E. Jørgensen <karl.jorgensen@recordsure.com>
2018-11-04 11:05:27 +0000

    Get external IP correct when there is not internet connection
    Returning None rather than the "magic" 0.0.0.0 is definitely more pythonic

--- virginmedia.py
+++ virginmedia.py
@@ -324,7 +324,9 @@ class Hub(object):
     @_snmpProperty("1.3.6.1.4.1.4115.1.20.1.1.1.7.1.3.1")
     def wanIPv4Address(self, snmpValue):
         """The current external IP address of the hub"""
-        return _extract_ip(snmpValue)
+        x = _extract_ip(snmpValue)
+        if x == "0.0.0.0": return None
+        else: return x
 
     @_snmpProperty("1.3.6.1.4.1.4115.1.20.1.1.1.11.2.1.3.1")
     def dns_servers(self, snmpValue):
