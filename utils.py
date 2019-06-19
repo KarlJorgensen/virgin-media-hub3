e5eb340aa5e3786e16d7d815dadb958fae5639eb
Karl E. Jørgensen <karl.jorgensen@recordsure.com>
2019-06-19 20:37:23 +0100

    Fix up debug wrapper
    yes: the debug wrapper needed some debugging..

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def debug(func):
     """A function decorator that will print function calls and their results"""
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        print("calling {0}(args={1}, kwargs={2})", func, args, kwargs)
+        print("calling {0}(args={1}, kwargs={2})".format(func, args, kwargs))
         res = func(*args, **kwargs)
         print("{f}(args={a}, kwargs={kw}) => {res}".format(f=func.__name__,
                                                            a=args,
